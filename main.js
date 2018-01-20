ed0a930cc6107332ac5f46bcb0ee0b19c56b4adc
samuel <samuel.mathes@gmx.de>
2018-01-20 14:24:23 +0100

    Initial Test

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+app.commandLine.appendSwitch('remote-debugging-port', '9222')
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
--- renderer.js
+++ renderer.js
@@ -1,3 +1,68 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+console.log(document)
+
+var Imap = require('imap'),
+    inspect = require('util').inspect;
+
+var imap = new Imap({
+    user: 'samuel.mathes@gmail.com',
+    password: '',
+    host: 'imap.gmail.com',
+    port: 993,
+    tls: true
+});
+
+function openInbox(cb) {
+    imap.openBox('INBOX', true, cb);
+    debugger;
+}
+
+imap.once('ready', function() {
+    openInbox(function(err, box) {
+        console.log("Total messages: " + box.messages.total)
+        console.log("New messages: " + box.messages.new)
+        if (err) throw err;
+        var f = imap.seq.fetch('4074:4075', {
+            bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+            struct: true
+        });
+        f.on('message', function(msg, seqno) {
+            console.log('Message #%d', seqno);
+            var prefix = '(#' + seqno + ') ';
+            msg.on('body', function(stream, info) {
+                var buffer = '';
+                stream.on('data', function(chunk) {
+                    buffer += chunk.toString('utf8');
+                });
+                stream.once('end', function() {
+                    console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+                });
+            });
+            msg.once('attributes', function(attrs) {
+                console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+            });
+            msg.once('end', function() {
+                console.log(prefix + 'Finished');
+            });
+        });
+        f.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+        });
+        f.once('end', function() {
+            console.log('Done fetching all messages!');
+            imap.end();
+        });
+    });
+});
+
+imap.once('error', function(err) {
+    console.log(err);
+});
+
+imap.once('end', function() {
+    console.log('Connection ended');
+});
+
+imap.connect();
